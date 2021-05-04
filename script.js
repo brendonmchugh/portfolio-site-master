ed6f2c62ea2eaf35123561718e5f5021452c86de
brendonmchugh <77760574+brendonmchugh@users.noreply.github.com>
2021-05-04 09:33:09 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+(function(){
+    var words = [
+        ' leader',
+        ' designer',
+        ' creater',
+        
+        ], i = 0;
+    setInterval(function(){
+        $('#changingword').fadeOut(function(){
+            $(this).html(words[i=(i+1)%words.length]).fadeIn();
+        });
+    }, 3000);
+      
+})();
+
